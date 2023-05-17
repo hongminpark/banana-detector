d1b1da7c7d60ab358597325e43a5e74248a79ad5
Hongmin Park <hongmin.park@disquiet.io>
2023-05-17 23:12:29 +0900

    Add why banana modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
-import DragAndDropImage from "./DragAndDropImage";
+// import DragAndDropImage from "./DragAndDropImage";
 ("../../node_modules/@types/estree/index.d");
 
 const bananas = [
@@ -24,7 +24,8 @@ const bananas = [
 ];
 
 const Home: NextPage = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showBananaModal, setShowBananaModal] = useState(false);
+  const [showWhyModal, setShowWhyModal] = useState(false);
   const [clickedId, setClickedId] = useState(0);
 
   return (
@@ -36,8 +37,9 @@ const Home: NextPage = () => {
       </Head>
       <main className="relative flex min-h-screen flex-col items-center">
         <h1 className="py-4 text-center text-5xl text-[6.5rem] font-extrabold leading-[5.5rem] tracking-tighter">
-          not banana
+          banana detector
         </h1>
+        <div className="px-4 text-center text-[1.4rem] leading-5 tracking-tight"></div>
         <div className="container flex flex-col items-center justify-center gap-24 px-4 py-8 ">
           <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
             {bananas.map((banana, i) => (
@@ -47,33 +49,61 @@ const Home: NextPage = () => {
                   src={banana}
                   onClick={() => {
                     setClickedId(i);
-                    setShowModal(true);
+                    setShowBananaModal(true);
                   }}
                 />
-                <div className="text-light py-4 text-center font-normal">{`anonymous banana ${
-                  i + 1
-                }`}</div>
+                <div className="text-light py-4 text-center font-normal">
+                  {i === 6 ? `Jenny's banana` : `anonymous banana ${i + 1}`}
+                </div>
               </div>
             ))}
           </div>
         </div>
-        <div className={"absolute"}>
+        {/* <div className={"absolute"}>
           <DragAndDropImage />
-        </div>
+        </div> */}
+        <button
+          onClick={() => setShowWhyModal(true)}
+          className="fixed bottom-8 right-8 border-2 border-black bg-white px-4 py-2 font-medium tracking-tighter text-black hover:border-white hover:bg-black hover:text-white"
+        >
+          Why banana ?
+        </button>
       </main>
-      {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center">
+      {showBananaModal && (
+        <div
+          className="fixed inset-0 flex items-center justify-center"
+          onClick={() => setShowBananaModal(false)}
+        >
           <div className="fixed inset-0 bg-black opacity-90" />
           <div className="max-w-200 relative w-3/5">
             <div className="p-4">
               <img
                 className="aspect-square w-full object-contain"
                 src={bananas[clickedId]}
-                onClick={() => setShowModal(false)}
               />
               <div className="py-4 text-center text-xl font-normal text-white">{`anonymous banana ${
                 clickedId + 1
               }`}</div>
+            </div>
+          </div>
+        </div>
+      )}
+      {showWhyModal && (
+        <div
+          onClick={() => setShowWhyModal(false)}
+          className="fixed inset-0 flex items-center justify-center"
+        >
+          <div className="fixed inset-0 bg-black opacity-90" />
+          <div className="max-w-200 relative w-3/5">
+            <div className="text-3xl font-medium italic leading-[2rem] text-white/80">
+              Banana is an everyday object transformed into a vibrant
+              masterpiece by artists like Andy Warhol and Maurizio. You can
+              discover the power of art that resides within the grasp of
+              everyone, as this humble fruit becomes a tool for artistic
+              expression that transcends boundaries. It's luminous hues,
+              delicate curves, and unique texture becomes a symbol of creativity
+              and a catalyst for embracing the artistic potential that lies
+              hidden in the world around us.
             </div>
           </div>
         </div>
